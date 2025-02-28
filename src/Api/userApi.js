8b164609fd5c3c63eda96bae29a57024ca94f3c3
Alina <alinaens2005@gmail.com>
2025-02-28 12:24:34 +0700

    подправлена страница входа

--- src/Api/userApi.js
+++ src/Api/userApi.js
@@ -4,37 +4,6 @@ const instance = axios.create({
     baseURL: 'https://blog.kreosoft.space/api/account/'
 });
 
-function registerUser(fullName, password, email, birthDate, gender, phoneNumber) {
-    const body = {
-        fullName: fullName,
-        password: password,
-        email: email,
-        birthDate: birthDate,
-        gender: gender,
-        phoneNumber: phoneNumber
-        /*firstName: firstName,
-        lastName: lastName,
-        middleName: middleName,
-        email: email,
-        password1: password1,
-        password2: password2*/
-    }
-
-    return instance.post(`register`, body)
-        .then(response => {
-            console.log("Catalog Data:", response.data);
-
-            if (response.status === 200) {
-                console.log("Catalog Data:", response.data);
-                return response.data;
-            }
-        })
-        .catch(error => {
-            console.log(error.response.data.error)
-        });
-}
-
-
 function loginUser(email, password){
     return instance.post("/login", { email, password })
     .then(response => {
@@ -48,6 +17,6 @@ function loginUser(email, password){
 }
 
 export const userApi = {
-    registerUser: registerUser,
+
     loginUser: loginUser
 }
--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import LoginUser from "./Components/login/loginUser";
 
 function RoutesPage() {
     return (
         <Routes>
             <Route path="/" />
-            <Route path="/login" />
+            <Route path="/login" element={<LoginUser />} />
             <Route path="/listStudents" />
             <Route path="/applications" />
             <Route path="/role" />

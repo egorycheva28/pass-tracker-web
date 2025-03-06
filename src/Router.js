c49a1be126c82b59828dd5366fe595a7289fca0c
egorycheva28 <egor.masha28@list.ru>
2025-03-06 11:13:02 +0700

    добавила пути для новых страниц

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import LoginUser from "./Components/login/loginUser";
+import RegisterUserContainer from "./Components/registration/registerUserContainer";
+import ListStudentsContainer from "./Components/teacher/listStudentsContainer";
+import GetUserProfile from "./Components/profil/getUserProfil";
 
 function RoutesPage() {
     return (
         <Routes>
-            <Route path="/" />
+            <Route path="/" element={<ListStudentsContainer />} />
             <Route path="/login" element={<LoginUser />} />
-            <Route path="/listStudents" />
+            <Route path="/profile" element={<GetUserProfile />} />
+            <Route path="/registration" element={<RegisterUserContainer />} />
+            <Route path="/listStudents" element={<ListStudentsContainer />} />
+            <Route path="/approvedApplications" element={<ListStudentsContainer/>} />
             <Route path="/applications" />
+            <Route path="/detail/:id" element={<GetUserProfile/>}/>
             <Route path="/role" />
         </Routes>
     )

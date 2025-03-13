efdf5f82e78c373240665c69a8b604822da8f5ad
egorycheva28 <egor.masha28@list.ru>
2025-03-13 11:50:07 +0700

    решение конфликта

--- src/Api/userApi.js
+++ src/Api/userApi.js
@@ -2,29 +2,29 @@ import createAxiosInstance from "./axiosInstance";
 
 const api = createAxiosInstance("user");
 
-function loginUser(email, password){
+function loginUser(email, password) {
     return api.post("/login", { email, password })
-    .then(response => {
-        if(response.status === 200){
-            localStorage.setItem('token', response.data.accessToken);
-            localStorage.setItem('refreshToken', response.data.refreshToken);
-            return response.data;
-        }
-    })
-    .catch(error => {
-        console.log(error.response.data?.error || "Ошибка входа");
-    });
+        .then(response => {
+            if (response.status === 200) {
+                localStorage.setItem('token', response.data.accessToken);
+                localStorage.setItem('refreshToken', response.data.refreshToken);
+                return response.data;
+            }
+        })
+        .catch(error => {
+            console.log(error.response.data?.error || "Ошибка входа");
+        });
 }
 
 function registerUser(fullName, password, email, birthDate, gender, phoneNumber) {
     return api.post("/register", { fullName, password, email, birthDate, gender, phoneNumber })
-    .then(response => {
-        if(response.status === 200){
-            localStorage.setItem('token', response.data.accessToken);
-            localStorage.setItem('refreshToken', response.data.refreshToken);
-            return response.data;
-        }
-    })
+        .then(response => {
+            if (response.status === 200) {
+                localStorage.setItem('token', response.data.accessToken);
+                localStorage.setItem('refreshToken', response.data.refreshToken);
+                return response.data;
+            }
+        })
         .catch(error => console.log(error.response.data?.error || "Ошибка регистрации"));
 }
 
@@ -34,13 +34,13 @@ function logout() {
         .catch(error => console.log(error.response.data?.error || "Ошибка выхода"));
 }
 
-function getProfile(){
+function getProfile() {
     return api.get("/profile")
         .then(response => response.data)
         .catch(error => console.log(error.response.data?.error || "Ошибка получения профиля"));
 }
 
-function updateProfile(fullName){
+function updateProfile(fullName) {
     return api.post("/profile", { fullName })
         .then(response => response.data)
         .catch(error => console.log(error.response.data?.error || "Ошибка обновления профиля"));

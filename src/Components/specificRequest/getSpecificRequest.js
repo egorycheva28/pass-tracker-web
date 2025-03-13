0504e6234c394016b817d1ab6ca8562703a55771
egorycheva28 <egor.masha28@list.ru>
2025-03-13 22:13:44 +0700

    подключила переход на страницу конкретной заявки

--- src/Components/specificRequest/getSpecificRequest.js
+++ src/Components/specificRequest/getSpecificRequest.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { requestApi } from "../../Api/specificRequestApi.js";
-import { deaneryApi } from '../../Api/deaneryApii.js';
+import { deaneryApi } from '../../Api/deaneryApi.js';
+import { useParams } from 'react-router-dom';
 
 const styles = {
   container: {
@@ -51,96 +52,98 @@ const styles = {
     objectFit: "cover",
     cursor: "pointer",
   },
-    modalOverlay1: {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.7)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      zIndex: 1000,
-    },
-    modalContent1: {
-      position: "relative",
-      backgroundColor: "white", 
-      flexDirection: "column",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "10px",
-      borderRadius: "8px",
-      
-    },
-    modalOverlay: {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.7)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      zIndex: 1000,
-    },
-    modalContent: {
-      position: "relative",
-      backgroundColor: "white",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "20px",
-      borderRadius: "8px",
-      width: "400px",
-    },
-    enlargedImage: {
-      width: "50vw", 
-      height: "80vh", 
-      objectFit: "contain", // Изображение не обрезается
-      borderRadius: "8px",
-    },
-    closeButton: {
-      position: "absolute",
-      top: "10px",
-      right: "10px",
-      background: "red",
-      color: "white",
-      border: "none",
-      borderRadius: "50%",
-      width: "30px",
-      height: "30px",
-      fontSize: "18px",
-      cursor: "pointer",
-    },
-    textarea: {
-      width: "100%",
-      height: "80px",
-      marginBottom: "10px",
-      padding: "10px",
-      borderRadius: "4px",
-      border: "1px solid #ccc",
-      fontSize: "14px",
-    },
+  modalOverlay1: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1000,
+  },
+  modalContent1: {
+    position: "relative",
+    backgroundColor: "white",
+    flexDirection: "column",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10px",
+    borderRadius: "8px",
 
-  
+  },
+  modalOverlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1000,
+  },
+  modalContent: {
+    position: "relative",
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px",
+    borderRadius: "8px",
+    width: "400px",
+  },
+  enlargedImage: {
+    width: "50vw",
+    height: "80vh",
+    objectFit: "contain", // Изображение не обрезается
+    borderRadius: "8px",
+  },
+  closeButton: {
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    background: "red",
+    color: "white",
+    border: "none",
+    borderRadius: "50%",
+    width: "30px",
+    height: "30px",
+    fontSize: "18px",
+    cursor: "pointer",
+  },
+  textarea: {
+    width: "100%",
+    height: "80px",
+    marginBottom: "10px",
+    padding: "10px",
+    borderRadius: "4px",
+    border: "1px solid #ccc",
+    fontSize: "14px",
+  },
+
+
 };
 
 function GetSpecificRequest() {
+  const { id } = useParams();
+
   const [error, setError] = useState("");
   const [user, setUser] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
+
   const [isDeclineModalOpen, setIsDeclineModalOpen] = useState(false);
   const [declineComment, setDeclineComment] = useState("");
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const data = await requestApi.getDetails(`d8f9d91d-f41c-471a-a94d-6cac8fadf2c5`);
+        const data = await requestApi.getDetails(id);
         if (data) {
           console.log("Успешная загрузка профиля", data);
           setUser(data);
@@ -159,7 +162,7 @@ function GetSpecificRequest() {
 
   const handleAccept = async () => {
     try {
-      await deaneryApi.acceptRequest(user?.id); // Предположим, что у пользователя есть id, используйте соответствующее поле
+      await deaneryApi.acceptRequest(id); // Предположим, что у пользователя есть id, используйте соответствующее поле
       console.log("Запрос принят");
       setUser((prevUser) => ({ ...prevUser, statusRequest: "Accepted" })); // Обновление состояния
     } catch (err) {
@@ -187,7 +190,7 @@ function GetSpecificRequest() {
 
   const handleDecline = async () => {
     try {
-      await deaneryApi.declineRequest(user?.id, { comment: declineComment });
+      await deaneryApi.declineRequest(id, declineComment);
       console.log("Запрос отклонён с комментарием:", declineComment);
       setUser((prevUser) => ({ ...prevUser, statusRequest: "Declined" }));
       setIsDeclineModalOpen(false);
@@ -195,7 +198,6 @@ function GetSpecificRequest() {
       setError(err.message || "Ошибка отклонения запроса");
     }
   };
-
 
   return (
     <div style={styles.container}>
@@ -205,21 +207,21 @@ function GetSpecificRequest() {
           <strong>Дата:</strong> {formatDate(user?.startDate)} - {formatDate(user?.finishDate)}
           {user?.typeRequest === "EducationalActivity" ? (
 
-                <button style={styles.button}>Продлить</button>
+            <button style={styles.button}>Продлить</button>
 
           ) : null}
-        
+
         </div>
         <div style={styles.infoRow}>
           <strong>Причина:</strong> {translations[user?.typeRequest] || user?.typeRequest || "Не указано"}
         </div>
 
-      <div style={styles.infoRow}>
+        <div style={styles.infoRow}>
           <strong>Причина:</strong> {translations[user?.statusRequest] || user?.statusRequest || "Не указано"}
         </div>
       </div>
 
-      
+
       <div style={styles.formBox}>
         <h2 style={{ marginBottom: "20px" }}>Описание:</h2>
         <div style={styles.infoRow}>{user?.comment || "Нет данных"}</div>
@@ -235,11 +237,11 @@ function GetSpecificRequest() {
         />
       </div>
       {user?.statusRequest === "Pending" ? (
-  <div>
-    <button style={styles.button} onClick={handleAccept}>Принять</button>
-    <button style={styles.declineButton } onClick={handleOpenDeclineModal}>Отклонить</button>
-  </div>
-) : null}
+        <div>
+          <button style={styles.button} onClick={handleAccept}>Принять</button>
+          <button style={styles.declineButton} onClick={handleOpenDeclineModal}>Отклонить</button>
+        </div>
+      ) : null}
       {isModalOpen && (
         <div style={styles.modalOverlay1} onClick={() => setIsModalOpen(false)}>
           <div style={styles.modalContent1} onClick={(e) => e.stopPropagation()}>
@@ -253,23 +255,23 @@ function GetSpecificRequest() {
         </div>
       )}
 
-{isDeclineModalOpen && (
-  <div style={styles.modalOverlay}>
-    <div style={styles.modalContent}>
-      <h3>Отклонить запрос</h3>
-      <textarea
-        style={styles.textarea}
-        value={declineComment}
-        onChange={(e) => setDeclineComment(e.target.value)}
-        placeholder="Введите комментарий..."
-      />
-      <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <button style={styles.button} onClick={handleDecline}>Подтвердить</button>
-        <button style={styles.declineButton} onClick={handleCloseDeclineModal}>Отмена</button>
-      </div>
-    </div>
-  </div>
-)}
+      {isDeclineModalOpen && (
+        <div style={styles.modalOverlay}>
+          <div style={styles.modalContent}>
+            <h3>Отклонить запрос</h3>
+            <textarea
+              style={styles.textarea}
+              value={declineComment}
+              onChange={(e) => setDeclineComment(e.target.value)}
+              placeholder="Введите комментарий..."
+            />
+            <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+              <button style={styles.button} onClick={handleDecline}>Подтвердить</button>
+              <button style={styles.declineButton} onClick={handleCloseDeclineModal}>Отмена</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import LoginUser from "./Components/login/loginUser";
-
 import RegisterUserContainer from "./Components/registration/registerUserContainer";
 import ListStudentsContainer from "./Components/teacher/listStudentsContainer";
 import GetUserProfile from "./Components/profil/getUserProfil";
@@ -8,20 +7,19 @@ import ApprovedApplicationsContainer from "./Components/deanery/firstPage/approv
 import UnapprovedApplicationsContainer from "./Components/deanery/secondPage/unapprovedContainer";
 import RoleContainer from "./Components/deanery/thirdPage/roleContainer";
 import ProfileById from "./Components/profil/profileById";
-
+import GetSpecificRequest from "./Components/specificRequest/getSpecificRequest";
 
 function RoutesPage() {
     return (
         <Routes>
             <Route path="/" element={<LoginUser />} />
             <Route path="/login" element={<LoginUser />} />
-
             <Route path="/profile" element={<GetUserProfile />} />
             <Route path="/registration" element={<RegisterUserContainer />} />
             <Route path="/listStudents" element={<ListStudentsContainer />} />
             <Route path="/approvedApplications" element={<ApprovedApplicationsContainer/>} />
             <Route path="/unapprovedApplications" element={<UnapprovedApplicationsContainer/>}/>
-            <Route path="/detail/:id" element={<GetUserProfile/>}/>
+            <Route path="/detail/:id" element={<GetSpecificRequest/>}/>
             <Route path="/profile/:id" element={<ProfileById/>}/>
             <Route path="/role" element={<RoleContainer/>}/>
 
